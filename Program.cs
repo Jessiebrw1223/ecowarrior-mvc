b87fd8b1de767859f4395b1bd942809a780e6344
Jessiebrw1223 <tu-email@example.com>
2026-04-24 20:16:52 -0500

    Enable database migrations on Render

--- Program.cs
+++ Program.cs
@@ -33,7 +33,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-     // db.Database.Migrate();
+    db.Database.Migrate();
     // SeedData.Initialize(db);
 }
 
